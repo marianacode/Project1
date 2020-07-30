91119c54e4112a27771a73aa9a52c7b168fce689
Karuna Sato <karutimes2@comcast.net>
2020-07-29 21:24:34 -0700

    modified travel ban css, js

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,17 @@
+$(document).ready(function)(){
+ let restaurants
 
-// variables 
-
-
-var destinationID = ''
-var locationID = 0;
-var cuisineID = 0;
+let queryURL = "https://developers.zomato.com/api/v2.1/search" + APIkey;
+let APIkey = "8ed72bc9e077393211fcb8e5f0153fbd";
 
 
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
 
+.then(function(response){
+    console.log(queryURL)
 
-
-// Start jQuery 
-
-
-
-// sideNav 
-
-
-
-$(document).ready(function(){
-    $('.sidenav').sidenav();
-  });
-        
-
-  $(document).ready(function(){
-    $('.slider').slider({
-        data: { 
-    indicators: false,
-    height: 500,
-    transition: 500,
-    interval: 6000
-    },
-  });
-});
-        
-// Autocomplete 
-
-$(document).ready(function(){
-    $('input.autocomplete').autocomplete({
-      data: {
-        "Rome": null,
-        "Paris": null,
-        "Rio de Janeiro": null,
-        "Cancun": null,
-        "Madrid": null,
-        "London": null,
-        "Turkey": null,
-        "Mykonos": null
-
-    },
-});
-});
-  
-
- 
-
-// $('.dropdown-trigger').dropdown();
-
+})   
+}
